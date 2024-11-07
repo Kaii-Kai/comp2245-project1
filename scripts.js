e3a6147a450d18154f1e201aeb8cf6fb0855b25f
Kyra Lee <500001736@my.fiveislands.uwi.edu>
2024-11-07 00:06:55 -0400

    Added java script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+
+const form = document.querySelector("form");
+const emailInput = document.getElementById("email");
+
+
+const validateInput = document.querySelector (".message") {
+	if (!emailInput.value) {
+		validateInput.textContent = "Please enter a valid email address.";
+	}
+	else {
+		validateInput.textContent = "Thank you! ${emailInput} Your email address has been added to our mailing list!";
+	}
+};
+
+form.addEventListener ("submit", (e) => {
+	e.preventDefault();
+	validateInput ();
+});
+
+emailInput.addEventListener ("input", () => {
+	validateInput ();
+});
